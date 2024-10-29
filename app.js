aef2d91ec7c9141cc31b2358fd8f6ad3f06e89e0
Khadafi <khadafiahmad01@gmail.com>
2024-10-29 17:20:01 +0700

    Load environment variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const connectDB = require("./backend/config/db");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
