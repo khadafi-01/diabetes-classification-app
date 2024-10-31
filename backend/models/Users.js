a491e97e5092b66c0a2f3e6b7c0cff5500b285a0
Khadafi <khadafiahmad01@gmail.com>
2024-10-31 17:16:31 +0700

    Adding Creation and Update Dates

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -17,6 +17,14 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // Hash the passwords before saving the user
