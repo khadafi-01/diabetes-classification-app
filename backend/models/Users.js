c0ce2ae5c14aa4b314068da661103ec71258956d
Khadafi <khadafiahmad01@gmail.com>
2024-10-31 17:08:30 +0700

    create email validation

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -7,6 +7,12 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+  },
   password: {
     type: String,
     required: true,
