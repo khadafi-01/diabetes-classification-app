a59c59a1f91d9738042ca8af4a04c8c5f10d0d0e
Khadafi <khadafiahmad01@gmail.com>
2024-11-02 08:44:58 +0700

    adding comment Middlware to verify JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const express = require("express");
 const router = express.Router();
 
 dotenv.config();
-
+// Middleware to verify JWT Tokens
 const auth = (req, res, next) => {
   const token = req.header("Authorization").replace("Bearer ", "");
   if (!token) {
