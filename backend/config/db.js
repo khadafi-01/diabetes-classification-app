18e42d8ad4c8ab7518d0e83296bfd6ff0883c700
Khadafi <khadafiahmad01@gmail.com>
2024-11-02 09:19:54 +0700

    Remove deprecated options in MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,11 +7,7 @@ const db = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(db);
     console.log("MongoDB connected ...");
   } catch (err) {
     console.error(err.message);
