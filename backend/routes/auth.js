db72fbbbf7f3193c61b61339f42a44447ad0bc34
Khadafi <khadafiahmad01@gmail.com>
2024-11-02 08:42:54 +0700

    Adding Middleware to verify JWT tokens

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,25 @@ const router = express.Router();
 
 dotenv.config();
 
+const auth = (req, res, next) => {
+  const token = req.header("Authorization").replace("Bearer ", "");
+  if (!token) {
+    return res.status(401).json({
+      message: "No token, authorization denied",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      message: "Token is not valid",
+    });
+  }
+};
+
 // Register
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
